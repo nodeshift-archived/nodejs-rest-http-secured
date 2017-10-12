857b1af218d7405ea64aeaee8a171c568ac36ba0
Lucas Holmquist <lholmqui@redhat.com>
2017-10-12 20:35:24 +0000

    update: fix tests

--- test/greeting-test.js
+++ test/greeting-test.js
@@ -1,15 +1,38 @@
 const test = require('tape');
 const supertest = require('supertest');
 
-const app = require('../app');
+const proxyquire = require('proxyquire');
+
+const MockedKC = function () {
+    return {
+      middleware: () => {
+        return (req, resp, next) => {
+          return next();
+        };
+      },
+      protect: () => {
+        return (req, resp, next) => {
+          return next();
+        };
+      }
+    };
+  };
+
+  const app = proxyquire('../app', {
+    'keycloak-connect': MockedKC
+  });
 
 test('test out greeting route with no query param', (t) => {
+
   supertest(app)
     .get('/api/greeting')
     .expect('Content-Type', /json/)
     .expect(200)
     .then(response => {
       t.equal(response.body.content, 'Hello, World');
+      t.end();
+    }).catch((err) => {
+      console.log(err);
       t.end();
     });
 });
